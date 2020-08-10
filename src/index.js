64ef2720f10b2ba024606c27d4c0afd18733e4da
Isabel Puerta Vega <Isa@iMac-de-Victor.local>
2020-08-10 17:00:06 +0200

    arreglo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 const sampleNumber = 1;
-<<<<<<< HEAD
 const sampleNumberB = 2;
-console.log(`Hello number ${sampleNumber}${sampleNumberB}`);
-=======
 const sampleNumberC = 3;
-console.log(`Hello number ${sampleNumber}${sampleNumberC}`);
->>>>>>> feature/display-number-c
+console.log(`Hello number ${sampleNumber}${sampleNumberB}${sampleNumberC}`);
